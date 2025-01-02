161c680d4303458ecbf15729b6a8167912203071
Miirmm <lefebvre.mael.76@gmail.com>
2025-01-02 17:13:22 +0100

    :3

---
+++ index.js
@@ -0,0 +1,153 @@
+gsap.to(".drop-m", {
+    duration:1.8,
+    ease: "bounce.out",
+    y: "60px",
+    delay: 0,
+});
+gsap.to(".drop-a", {
+    duration:1.8,
+    ease: "bounce.out",
+    y: "60px",
+    delay: 0.2,
+});
+gsap.to(".drop-e", {
+    duration:1.8,
+    ease: "bounce.out",
+    y: "60px",
+    delay: 0.4,
+});
+gsap.to(".drop-l", {
+    duration:1.8,
+    ease: "bounce.out",
+    y: "60px",
+    delay: 0.6,
+});
+gsap.to(".titre-nom", {
+    duration:1.8,
+    ease: "power4.out",
+    y: "60px",
+    delay: 0.8,
+});
+gsap.to( ".bordure-test",{
+    x: "100%",
+    duration:1.8,
+})
+gsap.to(".elastic-l", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "50px",
+    delay: 1,
+});
+gsap.to(".elastic-l", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "0",
+    delay: 2.3,
+});
+
+gsap.to(".elastic-e1", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "50px",
+    delay: 1.1,
+});
+gsap.to(".elastic-e1", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "0",
+    delay: 2.4,
+});
+
+gsap.to(".elastic-f", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "50px",
+    delay: 1.2,
+});
+gsap.to(".elastic-f", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "0",
+    delay: 2.5,
+});
+
+
+gsap.to(".elastic-e2", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "50px",
+    delay: 1.3,
+});
+gsap.to(".elastic-e2", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "0",
+    delay: 2.6,
+});
+
+
+gsap.to(".elastic-b", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "50px",
+    delay: 1.4,
+});
+gsap.to(".elastic-b", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "0",
+    delay: 2.7,
+});
+
+
+gsap.to(".elastic-v", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "50px",
+    delay: 1.5,
+});
+gsap.to(".elastic-v", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "0",
+    delay: 2.8,
+});
+
+
+gsap.to(".elastic-r", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "50px",
+    delay: 1.6,
+});
+gsap.to(".elastic-r", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "0",
+    delay: 2.9,
+});
+
+
+gsap.to(".elastic-e3", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "50px",
+    delay: 1.7,
+});
+gsap.to(".elastic-e3", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    x: "0",
+    delay: 3,
+});
+gsap.to(".test-button", {
+    duration:2.5,
+    ease: "elastic.out(1,0.4)",
+    y: "200%",
+    delay: 2.7,
+});
+
+
+
+
+
